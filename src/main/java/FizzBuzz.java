895c664a0155016dda622ee35b39ed15ac913dfd
Joshua Retallick <josh@old-empire.co.uk>
2021-06-10 14:22:50 +0100

    If number is divisble by 3 and 5, fizzbuzz returned

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,8 +2,10 @@ public class FizzBuzz {
 
     public String convert(int number) {
 
-        if (number % 3 == 0) {
-            return "fizz";
+        if (number % 3 == 0 && number % 5 == 0)
+            return "FizzBuzz";
+        else if (number % 3 == 0) {
+            return "Fizz";
         }
 
         return String.valueOf(number);
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,10 +1,12 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
     @Test
+    @DisplayName("If number is not divisible by 3 or 5 it is returned")
     void fizzBuzzLeavesNormalNumbersAlone() {
 
         FizzBuzz fizzBuzz = new FizzBuzz();
@@ -12,13 +14,27 @@ class FizzBuzzTest {
         Assertions.assertEquals("1", fizzBuzz.convert(1));
         Assertions.assertEquals("2", fizzBuzz.convert(2));
     }
-    
+
     @Test
+    @DisplayName("If number is divisible by 3 AND 5 FizzBuzz is returned")
+    void fizzBuzzConvertsMultiplesOfThreeAndFive() {
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        Assertions.assertEquals("FizzBuzz", fizzBuzz.convert(15));
+    }
+
+    @Test
+    @DisplayName("If number is divisble by 3 only Fizz is returned")
     void fizzBuzzConvertsMultiplesofThree() {
 
         FizzBuzz fizzBuzz = new FizzBuzz();
 
-        Assertions.assertEquals("fizz", fizzBuzz.convert(3));
+        Assertions.assertEquals("Fizz", fizzBuzz.convert(3));
 
+    }
+
+    @Test
+    void fizz() {
     }
 }

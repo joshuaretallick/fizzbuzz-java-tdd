638a6d894586a80a6a343caf9508559a35feb4f2
Joshua Retallick <josh@old-empire.co.uk>
2021-06-11 14:46:23 +0100

    Refactored

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
 
-    FizzBuzz enteredValue = new FizzBuzz();
-
     @Test
     @DisplayName("If number is not divisible by 3 or 5 it is returned")
     void fizzBuzzLeavesNormalNumbersAlone() {
+
+        FizzBuzz enteredValue = new FizzBuzz();
 
         Assertions.assertEquals("1", enteredValue.fizzBuzzConverter(1));
 
@@ -18,6 +18,8 @@ class FizzBuzzTest {
     @DisplayName("If number is divisible by 3 AND 5 FizzBuzz is returned")
     void fizzBuzzConvertsMultiplesOfThreeAndFive() {
 
+        FizzBuzz enteredValue = new FizzBuzz();
+
         Assertions.assertEquals("FizzBuzz", enteredValue.fizzBuzzConverter(15));
 
     }
@@ -25,6 +27,8 @@ class FizzBuzzTest {
     @Test
     @DisplayName("If number is divisible by 3 only Fizz is returned")
     void fizzBuzzConvertsMultiplesOfThree() {
+
+        FizzBuzz enteredValue = new FizzBuzz();
 
         Assertions.assertEquals("Fizz", enteredValue.fizzBuzzConverter(3));
 
@@ -34,6 +38,8 @@ class FizzBuzzTest {
     @Test
     @DisplayName("If number is divisible by 5 only Buzz is returned")
     void fizzBuzzConvertsMultiplesOfFive() {
+
+        FizzBuzz enteredValue = new FizzBuzz();
 
         Assertions.assertEquals("Buzz", enteredValue.fizzBuzzConverter(5));
 

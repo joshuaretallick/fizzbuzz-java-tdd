79bb72eeee895bf873ba5887d8742ecbe655f0a0
Joshua Retallick <josh@old-empire.co.uk>
2021-06-11 11:57:31 +0100

    Refactored to remove if/else

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,16 +3,13 @@ import java.util.Scanner;
 public class FizzBuzz {
 
     public String fizzBuzzConverter(int number) {
+        
+        if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
+        if (number % 3 == 0) return "Fizz";
+        if (number % 5 == 0) return "Buzz";
 
-        if (number % 3 == 0 && number % 5 == 0)
-            return "FizzBuzz";
-        else if (number % 3 == 0) {
-            return "Fizz";
-        }
-        else if (number % 5 == 0) {
-            return "Buzz";
-        }
         return String.valueOf(number);
+
     }
 
     public static void main(String[] args) {
